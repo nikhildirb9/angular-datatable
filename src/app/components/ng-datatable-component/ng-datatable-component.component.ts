902b1127dd3b32c86d6325c7e3c8ea61667b545c
nikhildirb9 <venkata.vanamamalai@gmail.com>
2020-04-21 12:02:57 -0700

    pagination is completed with user interaction

--- src/app/components/ng-datatable-component/ng-datatable-component.component.ts
+++ src/app/components/ng-datatable-component/ng-datatable-component.component.ts
@@ -10,6 +10,7 @@ export class NgDatatableComponentComponent implements OnInit {
   employees: object[];
   columns = ["name", "phone", "email", "company", "date_entry", "org_num", "address_1", "city", "zip", "geo", "status", "fee", "url", "date_first", "date_recent"];
   paginated_employees;
+  pages = 5;
   page = {
     number: 0,
     size: 10
@@ -18,12 +19,22 @@ export class NgDatatableComponentComponent implements OnInit {
 
   ngOnInit() {
     this.employees = employeeDetails;
+    this.pages = this.employees.length/this.page.size;
     this.setUpPagination()
   }
 
   setUpPagination() {
     const start = this.page.number * this.page.size;
     this.paginated_employees = this.employees.slice(start, start + this.page.size);
+  }
+
+  showPageData(pageNumber) {
+    this.page.number = pageNumber;
+    this.setUpPagination();
+  }
+
+  updateTable() {
+    this.setUpPagination();
   }
 
 }
